199032ecf471049fdc716b522f53eb9167f69caa
alber <alber@PC-ALBERTO>
2025-03-29 18:29:58 +0100

    Creamos el método sumar().

--- Calculadora/src/ejemplo/Operaciones.java
+++ Calculadora/src/ejemplo/Operaciones.java
@@ -2,6 +2,11 @@ package ejemplo;
 
 public class Operaciones {
 
+	// Operación sumar
+	public int sumar(int a, int b) {
+        return a + b;
+    }
+
 	public static void main(String[] args) {
 		// Proyecto calculadora
 
